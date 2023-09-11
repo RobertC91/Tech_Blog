3cf0db89cf5177039a7c47b4e1566055556e2c78
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-09-11 18:29:48 -0500

    testing deployment

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers.js')
 
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const sess = {
@@ -31,6 +28,7 @@ const sess = {
 };
 
 app.use(session(sess));
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 // Inform Express.js on which template engine to use
@@ -42,7 +40,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+app.listen(PORT, () => {
+  console.log('now listening')
+  sequelize.sync({
+    force: false
+  })
+})
